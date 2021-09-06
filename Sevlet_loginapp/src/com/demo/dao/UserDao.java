dbd203b03b9e9ceed680dae957acfea5b1c2d073
Aman Dua <amandua1010@gmail.com>
2021-09-06 23:24:22 +0530

    demo

--- Sevlet_loginapp/src/com/demo/dao/UserDao.java
+++
@@ -1,7 +0,0 @@
-package com.demo.dao;
-
-import com.demo.entity.User;
-
-public interface UserDao {
-	public User getUser(String username,String password);
-}
--- Sevlet_loginapp/src/com/demo/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.demo.service;
-
-import com.demo.entity.User;
-
-public interface UserService {
-	public User getUser(String username,String password);
-}
